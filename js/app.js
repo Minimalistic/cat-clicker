444bf5a798305d27ca1bd70857f60030b0a80aac
Jason Marsh <regularly.jason@gmail.com>
2017-05-24 14:02:22 -0500

    Feat: Two buttons for two separate cats
    Displays photo for each cat along with relevant info - doesn’t
    currently display tracked clicks.  Is also overly complicated for doing
    what it does in js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,69 @@
-document.getElementById("charlie_CatButton").onclick = displayCharlieCatInfo;
-document.getElementById("frida_CatButton").onclick = displayFridaCatInfo;
 
-var charlie_CatClicks = 0;
-document.getElementById("catName_Charlie").innerHTML = "Charlie";
-function displayCharlieCatInfo() {
-    charlie_CatClicks ++;
-    document.getElementById("charlie_CatClickCount").innerHTML = charlie_CatClicks;
+function pet(name, photo, owner, species, 
+             breed, gender, color, clicks) {
+                this.name = name;
+                this.photo = photo;
+                this.owner = owner;
+                this.species = species;
+                this.breed = breed;
+                this.gender = gender;
+                this.color = color;
+                this.clicks = clicks;
 }
 
-var frida_CatClicks = 0;
-document.getElementById("catName_Frida").innerHTML = "Frida";
-function displayFridaCatInfo() {
-    frida_CatClicks ++;
-    document.getElementById("frida_CatClickCount").innerHTML = frida_CatClicks;
+var animalCharlie = new pet("Charlie",
+                            "img/charlie_IMG_0231.jpg",
+                            "Jason Marsh",
+                            "Feline",
+                            "Ragdoll",
+                            "Male",
+                            "Orange & White",
+                            "0");
+
+
+var animalFrida = new pet("Frida",
+                          "img/frida_IMG_2093.jpg",
+                          "Jason Marsh",
+                          "Feline",
+                          "Ragdoll",
+                          "Female",
+                          "Brown & Tan",
+                          "0");
+
+
+document.getElementById("charlie_ListButton").onclick = displayAnimalCharlie;
+function displayAnimalCharlie() {
+    document.getElementById("active_Cat_Name").innerHTML =
+        animalCharlie.name;
+    document.getElementById("active_Cat_Photo").src =
+        animalCharlie.photo;
+    document.getElementById("active_Cat_Info").innerHTML =
+        "Owner Name: " + animalCharlie.owner + "<br>" +
+        "Species: " + animalCharlie.species + "<br>" +
+        "Breed: " + animalCharlie.breed + "<br>" +
+        "Sex: " +  animalCharlie.gender + "<br>" +
+        "Color: " + animalCharlie.color; 
+}
+
+document.getElementById("frida_ListButton").onclick = displayAnimalFrida;
+function displayAnimalFrida() {
+    document.getElementById("active_Cat_Name").innerHTML = 
+        animalFrida.name;
+    document.getElementById("active_Cat_Photo").src =
+        animalFrida.photo;
+    document.getElementById("active_Cat_Info").innerHTML = 
+        "Owner Name: " + animalFrida.owner + "<br>" +
+        "Species: " + animalFrida.species + "<br>" +
+        "Breed: " + animalFrida.breed + "<br>" +
+        "Sex: " +  animalFrida.gender + "<br>" +
+        "Color: " + animalFrida.color; 
+}
+
+document.getElementById("active_Cat_Photo").onclick = countClick;
+function countClick() {
+    if (animalCharlie.photo) {
+        animalCharlie.clicks ++;
+    } else if (animalFrida.photo) {
+               animalFrida.clicks ++;
+    }
 }
